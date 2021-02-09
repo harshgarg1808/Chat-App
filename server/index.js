be1d055cae802adc76d86708fdd16b90d18de93a
harshgarg1808 <harshgarg1808@gmail.com>
2021-02-10 00:31:46 +0530

    Basic Structure Done

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const socketio = require('socket.io');
+const http = require('http');
+const port = process.env.PORT || 5000;
+const router = require('./router');
+const app = express()
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(router)
+
+io.on('connection' , (socket)=>{
+    console.log("we have a new connection!!");
+    socket.on('disconnect' , ()=>{
+        console.log("User had left!!");
+    })
+})
+
+
+
+server.listen(port , ()=>{
+    console.log(`Server has started on Port ${port}`);
+})
